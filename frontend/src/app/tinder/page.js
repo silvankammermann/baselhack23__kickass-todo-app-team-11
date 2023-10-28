66a33f4861f50fb013a7a6a3d356037783d8bce0
hazzeldorn <jonas@ohodesign.ch>
2023-10-28 18:16:21 +0200

    feat: tinder swiper (WIP)

--- frontend/src/app/tinder/page.js
+++ frontend/src/app/tinder/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import TaskRow from "../components/TaskRow/TaskRow";
+import TinderSwiper from "../components/TinderSwiper/TinderSwiper";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.main__inner}>
-        <TaskRow taskName="Learn tinders" />
+        <TinderSwiper taskName="This is a swipeable task" />
       </div>
     </main>
   );
