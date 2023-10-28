eac579e60c16dac67bc84d3e657ff18181c6ee63
hazzeldorn <jonas@ohodesign.ch>
2023-10-29 01:42:41 +0200

    bugfix: update stack of left items

--- frontend/src/app/components/TinderSwiper/TinderSwiper.js
+++ frontend/src/app/components/TinderSwiper/TinderSwiper.js
@@ -2,30 +2,45 @@
 
 import styles from "./TinderSwiper.module.css";
 import TinderCard from "react-tinder-card";
-import React, { useMemo, useRef } from "react";
+import React, { useState, useMemo, useRef } from "react";
 
 const TinderSwiper = ({ tasks }) => {
+  // Initial state for remaining tasks.
+  const [remainingTasks, setRemainingTasks] = useState(tasks);
+
   const cardRefs = useMemo(
     () =>
-      Array(tasks.length)
+      Array(remainingTasks.length)
         .fill(0)
         .map((i) => React.createRef()),
-    [tasks]
+    [remainingTasks]
   );
 
   const swipe = (direction) => {
-    // swipe the topmost card.
-    const lastCardIndex = tasks.length - 1;
+    // Swipe the topmost card.
+    const lastCardIndex = remainingTasks.length - 1;
     if (cardRefs[lastCardIndex].current) {
       cardRefs[lastCardIndex].current.swipe(direction);
     }
+  };
+
+  // Callback for outOfFrame.
+  const handleOutOfFrame = (name) => {
+    setRemainingTasks((prevTasks) =>
+      prevTasks.filter((task) => task.name !== name)
+    );
   };
 
   return (
     <>
       <div className={styles.cardStack}>
         {tasks.map(({ id, name }, index) => (
-          <TinderCard key={id} className={styles.card} ref={cardRefs[index]}>
+          <TinderCard
+            key={id}
+            className={styles.card}
+            ref={cardRefs[index]}
+            onCardLeftScreen={() => handleOutOfFrame(name)}
+          >
             <div className={styles.container}>
               <span className="h1">{name}</span>
             </div>
@@ -34,7 +49,7 @@ const TinderSwiper = ({ tasks }) => {
       </div>
       <div className={styles.decissionTriggers}>
         <button
-          className="h2 circle bgRed colorWhite"
+          className="h2 button circle bgRed colorWhite"
           onClick={() => {
             swipe("left");
           }}
@@ -42,7 +57,7 @@ const TinderSwiper = ({ tasks }) => {
           Later
         </button>
         <button
-          className="h2 circle bgGreen colorWhite"
+          className="h2 button circle bgGreen colorWhite"
           onClick={() => {
             swipe("right");
           }}
