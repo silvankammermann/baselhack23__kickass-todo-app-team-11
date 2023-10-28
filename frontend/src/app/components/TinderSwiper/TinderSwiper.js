675e62b85d8e038bb023618346f583dfe36860a1
hazzeldorn <jonas@ohodesign.ch>
2023-10-29 00:53:35 +0200

    resolve merge conflicts

--- frontend/src/app/components/TinderSwiper/TinderSwiper.js
+++ frontend/src/app/components/TinderSwiper/TinderSwiper.js
@@ -7,11 +7,18 @@ import TinderCard from "react-tinder-card";
 const TinderSwiper = ({ taksId, taskName }) => {
   return (
     <>
-      <TinderCard>
-        <div className={styles.container}>
-          <span className="h1">{taskName}</span>
-        </div>
-      </TinderCard>
+      <div className={styles.cardStack}>
+        <TinderCard key="1" className={styles.card}>
+          <div className={styles.container}>
+            <span className="h1">{taskName}</span>
+          </div>
+        </TinderCard>
+        <TinderCard key="2" className={styles.card}>
+          <div className={styles.container}>
+            <span className="h1">{taskName}</span>
+          </div>
+        </TinderCard>
+      </div>
       <div className={styles.decissionTriggers}>
         <button className="h2 circle bgRed colorWhite">Later</button>
         <button className="h2 circle bgGreen colorWhite">Yes!</button>
