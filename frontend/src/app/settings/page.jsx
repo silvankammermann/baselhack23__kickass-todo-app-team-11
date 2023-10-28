9edd346b04bf0446c20ae1ebcc9b5e61c022418d
Xeno Suter <xeno.suter@students.fhnw.ch>
2023-10-28 23:00:40 +0200

    Fix profile setting page

--- frontend/src/app/settings/page.jsx
+++ frontend/src/app/settings/page.jsx
@@ -2,105 +2,95 @@
 
 import React, {useEffect, useState} from "react";
 import Image from "next/image";
-import {CircularProgress, Typography} from "@mui/material";
-import Grid from '@mui/material/Grid';
-import styles from "./page.module.css";
+import { CircularProgress, Typography, Grid } from "@mui/material";
+import TextField from '@mui/material/TextField';
+
 
 export default function Settings() {
-  const [userData, setUserData] = useState({
-    "characteristics": ["Musical", "Extrovert", "Social", "Ambivalent", "Humorous"],
-    "lastname": "Doe",
-    "name": "John",
-    "username": "john_doe"
-  });
-
-  const [characteristics, setCharacteristics] = useState([
-    "Extrovert",
-    "Night-Active",
-    "Social",
-    "Sportive",
-    "Perfectionist",
-    "Musical",
-    "Humorous",
-    "Calm",
-    "Ambivalent",
-  ]);
+  const [userData, setUserData] = useState({});
+  const [characteristics, setCharacteristics] = useState([]);
 
   const fetchCharacteristics = async () => {
     try {
       const response = await fetch("http://127.0.0.1:5000/get-characteristics");
-      const data = await response.json();
-      console.log(response);
-      console.log(data);
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
-      setCharacteristics(data); // This line sets the fetched data to the userData state
+      const data = await response.json();
+      setCharacteristics(data);
     } catch (error) {
-      console.error("Error fetching user data:", error);
+      console.error("Error fetching characteristics:", error);
     }
   };
 
   const fetchUserData = async () => {
     try {
       const response = await fetch("http://127.0.0.1:5000/get-user");
-      const data = await response.json();
-      console.log(response);
-      console.log(data);
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
-      setUserData(data); // This line sets the fetched data to the userData state
+      const data = await response.json();
+      setUserData(data);
     } catch (error) {
       console.error("Error fetching user data:", error);
     }
   };
 
   useEffect(() => {
-    // fetchUserData();
-    // fetchCharacteristics);
+    fetchUserData();
+    fetchCharacteristics();
   }, []);
 
-  let isActive = function (tempChar) {
-    console.log(tempChar);
-    console.log(userData.characteristics);
-    return userData.characteristics.includes(tempChar)
-  }
+  const isActive = (tempChar) => {
+    return userData.characteristics.includes(tempChar);
+  };
 
   return (
     <>
-      <h1 align="center">
-        Profile
-      </h1>
+      <h1 style={{ textAlign: "center" }}>Profile</h1>
 
-      {userData ? (
+      {userData && characteristics.length > 0 ? (
         <>
-          <div align={'center'}>
-            <Image width={100} height={100} src={"/assets/Kickass-Assets_character-sloth-profile.svg"}
-                   alt={"Profile Image"}/>
+          <div style={{ textAlign: "center" }}>
+            <Image
+              width={100}
+              height={100}
+              src="/images/sloth-profile.svg"
+              alt="Profile Image"
+            />
           </div>
 
-          <>Name: {userData.name}</>
+          <TextField
+            id="outlined-read-only-input"
+            label="Name"
+            defaultValue={userData.username}
+            InputProps={{
+              readOnly: true,
+            }}
+          />
 
           <Grid container spacing={4}>
             {characteristics.map((characteristic, index) => (
               <Grid item key={index} xs={4}>
-                <div
-                  className={`${styles.characteristic__box} ${
-                    isActive(characteristic) ? styles.characteristic__box__active : ""
-                  }`}
-                >
-                  <span className={styles.characteristic__box__text}>
-                    {characteristic}
-                  </span>
-                </div>
-
+                <Image
+                  width={400}
+                  height={400}
+                  src={`/images/${characteristic}${
+                    isActive(characteristic) ? "_active" : ""
+                  }.png`}
+                  alt="Icon"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                    maxWidth: "100%",
+                  }}
+                />
               </Grid>
             ))}
           </Grid>
         </>
       ) : (
-        <CircularProgress/>
+        <CircularProgress />
       )}
     </>
   );
