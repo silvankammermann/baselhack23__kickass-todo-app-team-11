c96c11fbc5cd9737f5086e3d96f3abe067ca151b
Ian Mackerracher <ian.mackerracher@gmail.com>
2023-10-28 14:39:43 +0200

    Added simple endpoint to get tasks

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,16 @@
 # save this as app.py
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/gettasks")
 def hello():
-    return "Hello, World!"
+    with open('dummy_data.json') as task_file:
+        tasks = task_file.read()
+    return tasks
+
+
+if __name__ == "__main__":
+    app.run()
+
