e196d92645adeab98d7f4d66f8bb87db20baf10b
hazzeldorn <jonas@ohodesign.ch>
2023-10-29 00:50:39 +0200

    feat: card swiper with multiple cards

--- frontend/src/app/components/TinderSwiper/TinderSwiper.js
+++ frontend/src/app/components/TinderSwiper/TinderSwiper.js
@@ -7,11 +7,18 @@ import TinderCard from "react-tinder-card";
 const TinderSwiper = ({ taksId, taskName }) => {
   return (
     <>
-      <TinderCard>
-        <div className={styles.container}>
-          <span className="h1">{taskName}</span>
-        </div>
-      </TinderCard>
+      <div className={styles.cardStack}>
+        <TinderCard key="1" className={styles.card}>
+          <div className={styles.container}>
+            <span className="h1">{taskName}</span>
+          </div>
+        </TinderCard>
+        <TinderCard key="2" className={styles.card}>
+          <div className={styles.container}>
+            <span className="h1">{taskName}</span>
+          </div>
+        </TinderCard>
+      </div>
       <div>
         <button>LEFT</button>
         <button>RIGHT</button>
