1b6c2d2d40efc9ba86bb922388b5a03a50288d0e
Ian Mackerracher <ian.mackerracher@gmail.com>
2023-10-28 16:13:20 +0200

    added task type to data model, task priority func

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,20 @@
 # save this as app.py
 from flask import Flask
 import json
+import task_handling
 
 app = Flask(__name__)
 
 @app.route("/gettasks")
 def hello():
-    with open('dummy_data.json') as task_file:
-        tasks = task_file.read()
-        # tasks = json.load(task_file)
+    # with open('dummy_data.json') as task_file:
+    #     tasks_str = task_file.read()
+    #     # tasks = json.load(task_file)
+    #     tasks = json.loads(tasks_str)
 
-        foo = json.loads(tasks)
-        print(foo)
+    sorted_tasks = task_handling.order_tasks()
 
-    return foo
+    return sorted_tasks
 
 
 if __name__ == "__main__":
