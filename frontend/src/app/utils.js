cc0c5df94d1973590e9641f81fd76cfa0534feb9
silvankammermann <56345764+silvankammermann@users.noreply.github.com>
2023-10-29 14:50:10 +0100

    Add bg classes

--- frontend/src/app/utils.js
+++ frontend/src/app/utils.js
@@ -1,8 +1,8 @@
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 const getBgClass = task => {
-  if (task.type === 0) return "bg0";
-  if (task.type === 1) return "bg1";
-  if (task.type === 2) return "bg2";
+  if (task.type === 0) return "cardImportanceLow";
+  if (task.type === 1) return "cardImportanceMedium";
+  if (task.type === 2) return "cardImportanceHigh";
   return "";
 }
 
