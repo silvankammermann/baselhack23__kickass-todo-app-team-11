eaa62b786a213c92c8e92ee8b52ca674d3eb4c7f
Flo <florian@ohodesign.ch>
2023-10-29 15:28:00 +0100

    edit: icon radmonize

--- frontend/src/app/components/TaskRow/TaskRow.js
+++ frontend/src/app/components/TaskRow/TaskRow.js
@@ -2,24 +2,30 @@ import styles from "./TaskRow.module.css";
 import RemoveIcon from '@mui/icons-material/Remove';
 import Image from "next/image";
 
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 const TaskRow = ({ taskName }) => {
+  const iconNumber = getRandomNumber(1, 3);
+
   return (
     <>
       <div className={`${styles.container} bgPatternOrange`}>
         <button className={`${styles.removeButton} h3 circle bgPurple colorWhite`}>
           <RemoveIcon className="colorWhite bgPurple circle" />
         </button>
+
         <Image
           // className={styles.logo}
           width={50}
           height={50}
-          src="/images/__importance-1.svg"
+          src={`/images/__importance-${iconNumber}.svg`}
           style={{
             height: "auto",
             display: "block",
             margin: "0 auto",
             width: "30px",
-            transformY: "20px",
           }}
           alt="Icon"
         />
