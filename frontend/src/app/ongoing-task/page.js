95d96dd99e796cd07abcf7ed677dba68931db697
Flo <florian@ohodesign.ch>
2023-10-29 14:59:24 +0100

    edit: add animation

--- frontend/src/app/ongoing-task/page.js
+++ frontend/src/app/ongoing-task/page.js
@@ -1,10 +1,42 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useLayoutEffect } from "react";
 import SuccessVideo from "@/app/components/SuccessVideo/SuccessVideo";
 import styles from "./page.module.css";
+import { gsap } from "gsap";
+
 
 export default function OngoingTask() {
+
+  useLayoutEffect(() => {
+    gsap.timeline()
+      .fromTo(`.${styles.container}`, {
+        scale: 0.7,
+        rotate: -10,
+      }, {
+        duration: 0.6,
+        rotate: 10,
+        scale: 1,
+        ease: "power3.in",
+      })
+      .to(`.${styles.container}`, {
+        duration: 1,
+        rotate: 5,
+        scale: 1,
+        ease: "power3.out",
+      });
+    gsap.fromTo(`.${styles.container}`, {
+      scale: 1,
+    }, {
+      duration: 1,
+      scale: 1.03,
+      ease: "power1.in",
+      yoyo: true,
+      repeat: -1,
+      delay: 1.6,
+    })
+  });
+
   const [isDone, setIsDone] = useState(false);
   const [currentTask, setCurrentTask] = useState({
     name: "Loading...",
@@ -48,4 +80,6 @@ export default function OngoingTask() {
       )}
     </>
   );
+
+
 }
