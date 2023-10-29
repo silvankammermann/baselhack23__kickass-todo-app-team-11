96e3a3570c5b2612e9bd32c9c118da93b11c75a2
hazzeldorn <jonas@ohodesign.ch>
2023-10-29 15:18:16 +0100

    bufgix: default keyword

--- frontend/src/app/utils.js
+++ frontend/src/app/utils.js
@@ -1,12 +1,10 @@
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-const getBgClass = task => {
+
+const getBgClass = (task) => {
   if (task.type === 0) return "cardImportanceLow";
   if (task.type === 1) return "cardImportanceMedium";
   if (task.type === 2) return "cardImportanceHigh";
   return "";
-}
-
-export default {
-  random,
-  getBgClass
 };
+
+export { random, getBgClass };
