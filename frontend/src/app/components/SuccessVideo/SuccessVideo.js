8c870355d890cc94d25dfb1da1be571cbb5dd2fb
silvankammermann <56345764+silvankammermann@users.noreply.github.com>
2023-10-29 13:04:58 +0100

    Add button to success page

--- frontend/src/app/components/SuccessVideo/SuccessVideo.js
+++ frontend/src/app/components/SuccessVideo/SuccessVideo.js
@@ -1,9 +1,16 @@
 import styles from './SuccessVideo.module.css';
+import Button from '../Button/Button';
+import Link from 'next/link';
 
 export default function SuccessVideo(src) {
   return (
-    <video className={styles.video} autoPlay muted>
-      <source src="/videos/success__productive-sloth.mp4" type="video/mp4" />
-    </video>
+    <>
+      <video className={styles.video} autoPlay muted>
+        <source src="/videos/success__productive-sloth.mp4" type="video/mp4" />
+      </video>
+      <Link href="/" className={styles.button}>
+        <Button icon="arrow" text="Next one" />
+      </Link>
+    </>
   );
 }
