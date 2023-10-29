02a13205cfbdcf69ffa807cb3d49f5f7bbe34841
hazzeldorn <jonas@ohodesign.ch>
2023-10-29 12:18:35 +0100

    success video

--- frontend/src/app/ongoing-task/page.js
+++ frontend/src/app/ongoing-task/page.js
@@ -1,29 +1,34 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import SuccessVideo from "@/app/components/SuccessVideo/SuccessVideo";
 import styles from "./page.module.css";
 
 export default function OngoingTask() {
+  const [isDone, setIsDone] = useState(false);
   const [currentTask, setCurrentTask] = useState({
     name: "Loading...",
     id: 0,
   });
 
   useEffect(() => {
-    setCurrentTask(sessionStorage.getItem("ongoing_task"));
-    console.log(currentTask);
+    setCurrentTask(JSON.parse(sessionStorage.getItem("ongoing_task")));
   }, []);
 
   const handleDoneClick = () => {
     const url = `http://localhost:5000/set-done/${currentTask.id}`;
-    fetch(url, {}).then((r) => r);
+    fetch(url, {
+      method: "POST",
+    }).then((response) => {
+      setIsDone(true);
+    });
   };
 
   return (
     <>
       <div className={styles.card}>
         <div className={styles.container}>
-          <span className="h1">...{currentTask.name}...</span>
+          <span className="h1">{currentTask.name}</span>
         </div>
         <button
           className={`h2 button circle bgGreen colorWhite ${styles.doneTrigger}`}
@@ -32,6 +37,15 @@ export default function OngoingTask() {
           Yes!
         </button>
       </div>
+      {isDone && (
+        <SuccessVideo
+          src=""
+          onClick={() => {
+            sessionStorage.removeItem("ongoing_task");
+            router.push("/");
+          }}
+        />
+      )}
     </>
   );
 }
