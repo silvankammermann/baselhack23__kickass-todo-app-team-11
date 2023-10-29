23b8c13d548398d39914ba0a0784f98c7971fa44
Matse <matthiasscherer7@gmail.com>
2023-10-29 13:47:47 +0100

    Add relative pathings

--- backend/user_handling.py
+++ backend/user_handling.py
@@ -1,7 +1,6 @@
 import json
-
+import os
 from flask import jsonify
-
 
 mock_characteristics = [
     "extrovert",
@@ -15,10 +14,18 @@ mock_characteristics = [
     "ambivalent",
 ]
 
+
 def get_user():
+    original_directory = os.getcwd()
+    new_directory = "/backend"
+    os.chdir(os.path.join(original_directory, new_directory))
     with open("user.json") as f:
         mock_user = json.load(f)
+
+    os.chdir(original_directory)
+
     return mock_user
+
 
 def increase_user_score(score=0):
     with open("user.json") as f:
@@ -26,6 +33,7 @@ def increase_user_score(score=0):
     mock_user["score"] += score
     with open('data.json', 'w') as f:
         json.dump(mock_user, f)
+
 
 def get_characteristics():
     return jsonify(mock_characteristics)
