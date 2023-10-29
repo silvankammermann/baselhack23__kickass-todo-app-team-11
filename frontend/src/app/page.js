8e3ccfc8c032d04d73ae2a198c9e44b6b721783c
hazzeldorn <jonas@ohodesign.ch>
2023-10-29 10:36:41 +0100

    feat: load tasks from backend

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,15 +3,37 @@
 import { useState, useEffect, createContext } from "react";
 import styles from "./page.module.css";
 import TinderSwiper from "@/app/components/TinderSwiper/TinderSwiper";
-
-import defaultTasks from "./testdata/tasks.js";
+import defaultTasks from "@/app/testdata/tasks.js";
 
 export default function Home() {
-  const [tasks, setTasks] = useState(defaultTasks);
+  const [tasks, setTasks] = useState([]);
+
+  // TODO: might need refactoring -> maybe move to
+  const fetchTaskData = async () => {
+    try {
+      const response = await fetch(`http://localhost:5000/gettasks`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      setTasks(defaultTasks);
+    }
+  };
+
+  useEffect(() => {
+    fetchTaskData();
+  }, []);
 
   return (
     <>
-      <TinderSwiper tasks={tasks} />
+      {tasks && tasks.length > 0 ? (
+        <TinderSwiper tasks={tasks} />
+      ) : (
+        <span className={`h1 ${styles.msgLoading}`}>loading...</span>
+      )}
     </>
   );
 }
