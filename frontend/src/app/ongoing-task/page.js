c4f4a4e0f33ac5a3512acf2d4bb4b7788e371d08
hazzeldorn <jonas@ohodesign.ch>
2023-10-29 15:33:51 +0100

    Update page.js

--- frontend/src/app/ongoing-task/page.js
+++ frontend/src/app/ongoing-task/page.js
@@ -6,36 +6,48 @@ import styles from "./page.module.css";
 import fetchUserData from "@/app/components/Api/UserApi";
 import { gsap } from "gsap";
 
-
 export default function OngoingTask() {
+  useEffect(() => {
+    setCurrentTask(JSON.parse(sessionStorage.getItem("ongoing_task")));
+    fetchUserData().then((r) => setUserData(r));
+  }, []);
 
   useLayoutEffect(() => {
-    gsap.timeline()
-      .fromTo(`.${styles.container}`, {
-        scale: 0.7,
-        rotate: -10,
-      }, {
-        duration: 0.6,
-        rotate: 10,
-        scale: 1,
-        ease: "power3.in",
-      })
+    gsap
+      .timeline()
+      .fromTo(
+        `.${styles.container}`,
+        {
+          scale: 0.7,
+          rotate: -10,
+        },
+        {
+          duration: 0.6,
+          rotate: 10,
+          scale: 1,
+          ease: "power3.in",
+        }
+      )
       .to(`.${styles.container}`, {
         duration: 1,
         rotate: 5,
         scale: 1,
         ease: "power3.out",
       });
-    gsap.fromTo(`.${styles.container}`, {
-      scale: 1,
-    }, {
-      duration: 1,
-      scale: 1.03,
-      ease: "power1.in",
-      yoyo: true,
-      repeat: -1,
-      delay: 1.6,
-    })
+    gsap.fromTo(
+      `.${styles.container}`,
+      {
+        scale: 1,
+      },
+      {
+        duration: 1,
+        scale: 1.03,
+        ease: "power1.in",
+        yoyo: true,
+        repeat: -1,
+        delay: 1.6,
+      }
+    );
   });
 
   const [isDone, setIsDone] = useState(false);
@@ -44,11 +56,6 @@ export default function OngoingTask() {
     name: "Loading...",
     _id: 0,
   });
-
-  useEffect(() => {
-    setCurrentTask(JSON.parse(sessionStorage.getItem("ongoing_task")));
-    fetchUserData().then((r) => setUserData(r));
-  }, []);
 
   const handleDoneClick = () => {
     const url = `http://localhost:5000/set-done/${currentTask._id}`;
@@ -61,30 +68,36 @@ export default function OngoingTask() {
 
   return (
     <>
-      <div className={styles.card}>
-        <div className={styles.container}>
-          <span className="h1">{currentTask.name}</span>
-        </div>
-        <button
-          className={`h2 button circle bgGreen colorWhite ${styles.doneTrigger}`}
-          onClick={handleDoneClick}
-        >
-          DONE!
-        </button>
-      </div>
-      {isDone && (
-        <SuccessVideo
-          src={userData.score >= 5
-            ? "/videos/success__busy-beaver.mp4"
-            : "/videos/success__productive-sloth.mp4"}
-          onClick={() => {
-            sessionStorage.removeItem("ongoing_task");
-            router.push("/");
-          }}
-        />
+      {currentTask && userData ? (
+        <>
+          <div className={styles.card}>
+            <div className={styles.container}>
+              <span className="h1">{currentTask.name}</span>
+            </div>
+            <button
+              className={`h2 button circle bgGreen colorWhite ${styles.doneTrigger}`}
+              onClick={handleDoneClick}
+            >
+              DONE!
+            </button>
+          </div>
+          {isDone && (
+            <SuccessVideo
+              src={
+                userData.score >= 5
+                  ? "/videos/success__busy-beaver.mp4"
+                  : "/videos/success__productive-sloth.mp4"
+              }
+              onClick={() => {
+                sessionStorage.removeItem("ongoing_task");
+                router.push("/");
+              }}
+            />
+          )}
+        </>
+      ) : (
+        <CircularProgress />
       )}
     </>
   );
-
-
 }
