3306147af5ed021f80b6dbf605ff84649cbcb1c9
hazzeldorn <jonas@ohodesign.ch>
2023-10-29 07:07:05 +0100

    include logo in header (WIP)

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -6,34 +6,35 @@ import "@/app/fonts/Tanker/Fonts/WEB/css/tanker.css";
 import styles from "@/app/page.module.css";
 import Header from "./components/Header/Header";
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 /*
 export const metadata = {
   title: "Kick Ass Todo App",
   description: "A Todo App where your todo's won't be forgotten.",
 };
-
-let colorClass = "";
-/*
-if (usePathname() === "/tinder") {
-  colorClass = "bg--purple";
-}
 */
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    // i have absolutelly no idea how nextJS and routing stuff works
-    // therefore i inserted this little hacky effect here
-    if (window.location.pathname === "/tinder") {
-      document.body.classList.add("bgPurple");
-      document.body.classList.add("tinderPage");
-    }
-  }, []);
+  const pathname = usePathname();
+
+  // set body classes
+  let bodyClasses = "bgDarkPurple";
+  if (pathname === "/tinder") {
+    bodyClasses = "bgPurple tinderPage";
+  }
+
+  // define header content
+  let headerTitle = "";
+  if (pathname === "/") {
+    headerTitle = "My tasks";
+  }
 
   return (
     <html lang="en">
-      <body className="bgDarkPurple">
-        <Header title={`My tasks`} />
+      <body className={bodyClasses}>
+        <Header title={headerTitle} />
+        <b>{pathname}</b>
         <main className={styles.main}>
           <div className={styles.main__inner}>{children}</div>
         </main>
